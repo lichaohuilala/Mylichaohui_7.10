74af7b3a8ec83d4f330e38f4f85692e69a824537
lichaohuilala <578078578@qq.com>
2017-07-10 09:38:54 +0800

    测试

--- app/src/main/java/com/example/lichaohui/mylichaohui_710/MainActivity.java
+++ app/src/main/java/com/example/lichaohui/mylichaohui_710/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.lichaohui.mylichaohui_710;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.lichaohui.mylichaohui_710.view.AmountView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 提交
+        AmountView amount = (AmountView) findViewById(R.id.amount_view);
+        amount.setGoods_storage(20);
+        amount.setOnAmountChangeListener(new AmountView.OnAmountChangeListener() {
+            @Override
+            public void onAmountChange(View view, int amount) {
+                Toast.makeText(MainActivity.this,""+amount,Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/lichaohui/mylichaohui_710/view/OnAmountChangeListener.java
@@ -0,0 +1,11 @@
+package com.example.lichaohui.mylichaohui_710.view;
+
+import android.view.View;
+
+/**
+ * Created by lichaohui on 2017/7/10.
+ */
+
+public interface OnAmountChangeListener {
+    void onAmountChange(View view, int amount);
+}
